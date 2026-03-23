b8978e605c4bba27d8fb6f4a5bcd9363748c8d0c
Tyler Flynn <106820630+TJFlynn05@users.noreply.github.com>
2026-03-23 16:09:34 +0000

    [Don't merge] - Saving to recompile project

---
+++ Spellfield/Source/Spellfield/Private/DebugDataGrabber.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DebugDataGrabber.h"
+#include <fstream>
+#include <iostream>
+
+#include "Structs/CardStats.h"
+
+void UDebugDataGrabber::LogDebugData(FDebugLogData DataToPull, int PlayerID)
+{
+	auto NameOfClass = DataToPull.Stats;
+	DataToPull.Stats->UpgradeParameters.UpgradeName;
+	DataToPull.Stats->UpgradeParameters.UpgradeName;
+	DataToPull.Stats->UpgradeParameters.UpgradeName;
+	
+	
+	UE_LOG(LogClass, Display, TEXT("Log class"));
+	UE_LOG(LogBlueprint, Display, TEXT("Log Blueprint"));
+}
+
+void UDebugDataGrabber::WriteAllSavedDebugDataToFile(FString FileName)
+{
+	std::fstream file;
+	file.open("FileName", std::ios::in);
+	for (auto Data : DebugData)
+	{
+		file << "test" << std::endl;
+		file.close();
+	}
+}
+
+void UDebugDataGrabber::ClearLogSavedData()
+{
+	for (int i = 0; i < 4; ++i)
+	{
+		DebugData[i] = new FDebugLogData;
+	}
+}
---
+++ Spellfield/Source/Spellfield/Public/DebugDataGrabber.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "DebugDataGrabber.generated.h"
+
+struct FCardStats;
+/**
+ * 
+ */
+USTRUCT()
+struct FDebugLogData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	int8 playerIDAsIndex;
+	int8 playerIDThatWon;
+	
+	UObject* ObjectData;
+	int8 RoundsWon;
+	int8 RoundCounter;
+
+	//rounds/times picked (pick rate)
+	int8 NumberOfTimesCardPicked;
+	
+	//rounds won/times
+	int8 NumberOfTimesWonWithCard;
+	FCardStats* Stats;
+	
+
+	
+	
+};
+
+
+UCLASS()
+class SPELLFIELD_API UDebugDataGrabber : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+	FDebugLogData* DebugData[3]; //array set to match max player count of 4
+	
+	UFUNCTION(BlueprintCallable)
+	void LogDebugData(FDebugLogData DataToPull, int PlayerID);
+
+	UFUNCTION(BlueprintCallable)
+	void WriteAllSavedDebugDataToFile(FString FileName);
+
+	UFUNCTION()
+	void ClearLogSavedData();
+};
