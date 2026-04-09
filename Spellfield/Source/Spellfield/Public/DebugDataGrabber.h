451167e7cb1b24d139326c8f8ecf941156738243
Tyler Flynn <106820630+TJFlynn05@users.noreply.github.com>
2026-04-09 13:40:14 +0100

    Revert "Merge branch 'tech-crystals' into Tech"
    This reverts commit e83d2ad9596e31d56ef0f87dd81006bbf1666a29, reversing
    changes made to 04d2f10521daaa23ee50276b94ca242d4d20b998.

---
+++ Spellfield/Source/Spellfield/Public/DebugDataGrabber.h
@@ -0,0 +1,85 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Structs/CardStats.h"
+#include "DebugDataGrabber.generated.h"
+
+/**
+ * 
+ */
+USTRUCT()
+struct FDebugPlayerCardData : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	int TimesCardPicked = 0;
+	int TimesWonWithCard = 0;
+};
+
+
+USTRUCT(BlueprintType)
+struct FDebugLogData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite)
+	int PlayerID = 0;
+	
+	UPROPERTY(BlueprintReadWrite)
+	int RoundsWon = 0;
+	
+	UPROPERTY(BlueprintReadWrite)
+	FCardStats Stats;
+
+	//card is the key and value is the number of times won with card
+	UPROPERTY(BlueprintReadWrite)
+	TMap<FString, FDebugPlayerCardData> PlayerCards;
+	
+};
+
+UCLASS()
+class SPELLFIELD_API UDebugDataGrabber : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+	private:
+	UPROPERTY()
+	FDebugLogData DebugData[4]; //array set to match max player count of 4
+
+	UPROPERTY()
+	TMap<FString, FDebugPlayerCardData> OverallCardsChosen;
+
+	UPROPERTY()
+	int RoundCounter = 1;
+
+	UPROPERTY()
+	int GameCount = 1;
+	
+	/**Do Not Use*/
+	UFUNCTION(BlueprintCallable, meta=(DevelopmentOnly))
+	void LogDebugData(FDebugLogData DataToPull);
+
+	/**Writes all debug data to a specified file
+	* @param FileName specifies the file the data should be saved as
+	*/
+	UFUNCTION(BlueprintCallable)
+	void WriteAllSavedDebugDataToFile();
+
+	/**Clears all data from the debug variables*/
+	UFUNCTION(BlueprintCallable, meta=(DevelopmentOnly))
+	void ClearLogSavedData();
+
+	/**Updates all card information associated to each player*/
+	UFUNCTION(BlueprintCallable, meta=(DevelopmentOnly))
+	void UpdateCardDataForPlayerOnRoundBasis(FCardStats Card, int PlayerID, bool PlayerWon);
+
+	UFUNCTION(BlueprintCallable)
+	void OnGameEndPrintAllData();
+
+	UFUNCTION()
+	void FetchAllAvailableDebugData();
+	
+};
